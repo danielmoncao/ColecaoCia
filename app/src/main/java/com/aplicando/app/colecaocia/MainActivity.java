5d1112f99699b5b0fff380e8a51b77bbd8ad6032
daniel moncao <danielcarvalhomoncao@gmail.com>
2018-03-01 10:11:05 -0300

    app  guy SP

--- app/src/main/java/com/aplicando/app/colecaocia/MainActivity.java
+++ app/src/main/java/com/aplicando/app/colecaocia/MainActivity.java
@@ -60,7 +60,7 @@ public class MainActivity extends AppCompatActivity {
         botaolocalizacao.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this, MapsActivity.class));
+                startActivity(new Intent(MainActivity.this, MapaActivity.class));
             }
         });
 
---
+++ app/src/main/java/com/aplicando/app/colecaocia/MapaFragment.java
@@ -0,0 +1,55 @@
+package com.aplicando.app.colecaocia;
+
+import android.location.Address;
+import android.location.Geocoder;
+import android.os.Bundle;
+
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+
+import java.io.IOException;
+import java.util.List;
+
+/**
+ * Created by User on 01/03/2018.
+ */
+
+public class MapaFragment extends SupportMapFragment implements OnMapReadyCallback {
+
+    @Override
+    public void onCreate(Bundle bundle) {
+        super.onCreate(bundle);
+
+        getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        LatLng posicaoEmpresa = pegaCoordenadadoEndereco("Rua quatorze de julho 24A são joaquim Araçatuba São Paulo");
+        if (posicaoEmpresa != null) {
+            CameraUpdate update = CameraUpdateFactory.newLatLngZoom(posicaoEmpresa, 19);
+            googleMap.moveCamera(update);
+        }
+    }
+
+    private LatLng pegaCoordenadadoEndereco(String endereco) {
+        try {
+            Geocoder geocoder = new Geocoder(getContext());
+            List<Address> resultados =
+                    geocoder.getFromLocationName(endereco, 1);
+            if (!resultados.isEmpty()) {
+                LatLng posicao = new LatLng(resultados.get(0).getLatitude(), resultados.get(0).getLongitude());
+                return posicao;
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
+
